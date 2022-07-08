72a34044f2862c4cb8ef3cfa1c1c194ebfb8924c
MickaelAustoni <mickael.austoni@gmail.com>
2022-07-09 01:25:00 +0200

    feat: add tree reducer remove/replace

--- src/features/DecisionTreeGenerator/reducer/treeReducer.ts
+++ src/features/DecisionTreeGenerator/reducer/treeReducer.ts
@@ -1,9 +1,11 @@
-import { appendProps } from "find-and";
+import { appendProps, removeObject, replaceObject } from "find-and";
 import type { RawNodeDatum } from "react-d3-tree/lib/types/common";
 
 export const treeReducerActionType = {
   appendTreeCard: "appendTreeCard",
-};
+  removeTreeCard: "removeTreeCard",
+  replaceTreeCard: "replaceTreeCard",
+} as const;
 
 export const appendTreeCard = (name: string, children: RawNodeDatum[]) => ({
   children,
@@ -11,10 +13,25 @@ export const appendTreeCard = (name: string, children: RawNodeDatum[]) => ({
   type: treeReducerActionType.appendTreeCard,
 });
 
+export const removeTreeCard = (name: string) => ({
+  name,
+  type: treeReducerActionType.removeTreeCard,
+});
+
+export const replaceTreeCard = (name: string, children: RawNodeDatum) => ({
+  children,
+  name,
+  type: treeReducerActionType.replaceTreeCard,
+});
+
 const treeReducer = (state: any, action: any) => {
   switch (action.type) {
     case treeReducerActionType.appendTreeCard:
       return appendProps(state, { name: action.name }, { children: action.children });
+    case treeReducerActionType.removeTreeCard:
+      return removeObject(state, { name: action.name });
+    case treeReducerActionType.replaceTreeCard:
+      return replaceObject(state, { name: action.name }, action.children);
     default:
       throw new Error();
   }
