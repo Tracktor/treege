fcc4f15e4e77c5c42753239c53a5ee3fa7f9d40e
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-17 00:25:35 +0200

    refactor: minor change

--- src/features/DecisionTreeGenerator/components/TreeGrid/useTreeGrid.tsx
+++ src/features/DecisionTreeGenerator/components/TreeGrid/useTreeGrid.tsx
@@ -1,13 +1,16 @@
-import { useContext } from "react";
+import { useCallback, useContext, useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import { DecisionTreeGeneratorContext } from "@/features/DecisionTreeGenerator/context/DecisionTreeGeneratorContext";
+import { setTree } from "@/features/DecisionTreeGenerator/reducer/treeReducer";
 
 const useTreeGrid = () => {
   const { t } = useTranslation("modal");
-  const { tree, currentHierarchyPointNode, modalOpen, setModalOpen } = useContext(DecisionTreeGeneratorContext);
+  const { tree, currentHierarchyPointNode, modalOpen, dispatchTree, setModalOpen } = useContext(DecisionTreeGeneratorContext);
   const isEditModal = modalOpen === "edit";
   const isAddModal = modalOpen === "add";
   const isModalMutationOpen = isEditModal || isAddModal;
+
+  const closeModal = () => setModalOpen(null);
 
   const getTitleModalMutation = () => {
     const name = currentHierarchyPointNode?.data.name;
@@ -21,11 +24,26 @@ const useTreeGrid = () => {
     return t("deleteTitle", { name });
   };
 
-  const closeModal = () => setModalOpen(null);
-
   const handleOnSave = () => {
     window.parent.postMessage({ source: "treege", tree, type: "onSave" }, "*");
   };
+
+  const handleMessage = useCallback(
+    (event: MessageEvent) => {
+      if (event.data?.source === "treege" && event.data?.type === "initTree") {
+        dispatchTree(setTree(event.data?.tree));
+      }
+    },
+    [dispatchTree]
+  );
+
+  useEffect(() => {
+    window.addEventListener("message", handleMessage);
+
+    return () => {
+      window.removeEventListener("message", handleMessage);
+    };
+  }, [handleMessage]);
 
   return { closeModal, getTitleModalDelete, getTitleModalMutation, handleOnSave, isModalMutationOpen };
 };
--- src/features/DecisionTreeGenerator/context/DecisionTreeGeneratorProvider.tsx
+++ src/features/DecisionTreeGenerator/context/DecisionTreeGeneratorProvider.tsx
@@ -1,6 +1,6 @@
-import { ReactNode, useEffect, useMemo, useReducer, useState } from "react";
+import { ReactNode, useMemo, useReducer, useState } from "react";
 import { DecisionTreeGeneratorContext, treeDefaultValue } from "@/features/DecisionTreeGenerator/context/DecisionTreeGeneratorContext";
-import treeReducer, { setTree } from "@/features/DecisionTreeGenerator/reducer/treeReducer";
+import treeReducer from "@/features/DecisionTreeGenerator/reducer/treeReducer";
 
 interface DecisionTreeGeneratorContextProviderProps {
   children: ReactNode;
@@ -10,20 +10,6 @@ const DecisionTreeGeneratorContextProvider = ({ children }: DecisionTreeGenerato
   const [currentHierarchyPointNode, setCurrentHierarchyPointNode] = useState(treeDefaultValue.currentHierarchyPointNode);
   const [modalOpen, setModalOpen] = useState(treeDefaultValue.modalOpen);
   const [tree, dispatchTree] = useReducer(treeReducer, treeDefaultValue.tree);
-
-  const handleMessage = (event: MessageEvent) => {
-    if (event.data?.source === "treege" && event.data?.type === "initTree") {
-      dispatchTree(setTree(event.data?.tree));
-    }
-  };
-
-  useEffect(() => {
-    window.addEventListener("message", handleMessage, false);
-
-    return () => {
-      window.removeEventListener("message", handleMessage);
-    };
-  }, []);
 
   const value = useMemo(
     () => ({
