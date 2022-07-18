b2fbb14b1d6c3922cff75632cb46d78ee11b2e12
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-18 11:53:48 +0200

    refactor: button create tree

--- src/features/DecisionTreeGenerator/components/ButtonCreateTree/ButtonCreateTree.tsx
+++ src/features/DecisionTreeGenerator/components/ButtonCreateTree/ButtonCreateTree.tsx
@@ -1,17 +1,19 @@
-import AddBoxRoundedIcon from "@mui/icons-material/AddBoxRounded";
+import AccountTreeRoundedIcon from "@mui/icons-material/AccountTreeRounded";
 import { Box, Button } from "@mui/material";
 import { useContext } from "react";
+import { useTranslation } from "react-i18next";
 import { DecisionTreeGeneratorContext } from "@/features/DecisionTreeGenerator/context/DecisionTreeGeneratorContext";
 
 const ButtonCreateTree = () => {
+  const { t } = useTranslation("button");
   const { setModalOpen } = useContext(DecisionTreeGeneratorContext);
 
   const handleClick = () => setModalOpen("add");
 
   return (
     <Box alignItems="center" justifyContent="center" display="flex" height="100%">
-      <Button onClick={handleClick} size="large">
-        <AddBoxRoundedIcon />
+      <Button onClick={handleClick} size="medium" variant="contained" startIcon={<AccountTreeRoundedIcon />}>
+        {t("createTree")}
       </Button>
     </Box>
   );
