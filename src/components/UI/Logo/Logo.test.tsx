b2a75d090545d1ea751ec43e9e44ac222592e219
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-20 09:22:28 +0200

    test: logo

--- src/components/UI/Logo/Logo.test.tsx
+++ src/components/UI/Logo/Logo.test.tsx
@@ -4,6 +4,11 @@ import Logo from "./Logo";
 
 test("render logo", () => {
   render(<Logo />);
+
   const img = screen.getByRole("img");
+
   expect(img).toBeInTheDocument();
+  expect(img).toHaveAttribute("alt", "Treege");
+  expect(img).toHaveAttribute("height", "30");
+  expect(img).toHaveAttribute("width", "auto");
 });
