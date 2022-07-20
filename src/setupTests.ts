4a37fa7271007d0bf37869b90a5110bad4f2403c
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-20 16:40:39 +0200

    test: removeObjectProperty

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -3,3 +3,13 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
+
+// Mock translation
+vi.mock("react-i18next", () => ({
+  useTranslation: () => ({
+    i18n: {
+      changeLanguage: () => new Promise(() => {}),
+    },
+    t: (str: string) => str,
+  }),
+}));
