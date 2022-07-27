bb1dca7820bc0323483327f8f3e6e23162f5c045
mickaelaustoni <mickael.austoni@gmail.com>
2022-07-27 17:09:44 +0200

    lock file

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,11 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      jsxRuntime: "classic",
+    }),
+  ],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
