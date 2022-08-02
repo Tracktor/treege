80179fbc80e1df2238a55ed3a1c9dfbf61753e64
mickaelaustoni <mickael.austoni@gmail.com>
2022-08-02 14:31:34 +0200

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   plugins: [
