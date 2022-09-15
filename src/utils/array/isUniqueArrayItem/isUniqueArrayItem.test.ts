a39be50024303184ad817ff30dc72d4855e7ac55
badreleglil <badre.leglil@gmail.com>
2022-09-15 15:32:40 +0200

    add excludeEntry attribute in isUniqueArrayItemWithNewEntry()

--- src/utils/array/isUniqueArrayItem/isUniqueArrayItem.test.ts
+++ src/utils/array/isUniqueArrayItem/isUniqueArrayItem.test.ts
@@ -35,4 +35,31 @@ describe("isUniqueArrayItemWithNewEntry", () => {
     const result = isUniqueArrayItemWithNewEntry(array, name);
     expect(result).toEqual(false);
   });
+
+  test("edit name with unique name", () => {
+    const array = ["name", "gender"];
+    const name = "age";
+    const currentName = "gender";
+
+    const result = isUniqueArrayItemWithNewEntry(array, name, currentName);
+    expect(result).toEqual(true);
+  });
+
+  test("edit name with same name editable", () => {
+    const array = ["name", "gender"];
+    const name = "gender";
+    const currentName = "gender";
+
+    const result = isUniqueArrayItemWithNewEntry(array, name, currentName);
+    expect(result).toEqual(true);
+  });
+
+  test("edit name with name exist in tree", () => {
+    const array = ["name", "gender"];
+    const name = "name";
+    const currentName = "gender";
+
+    const result = isUniqueArrayItemWithNewEntry(array, name, currentName);
+    expect(result).toEqual(false);
+  });
 });
--- src/utils/array/isUniqueArrayItem/isUniqueArrayItem.ts
+++ src/utils/array/isUniqueArrayItem/isUniqueArrayItem.ts
@@ -3,4 +3,10 @@ export const isUniqueArrayItem = (array: string[]): boolean => {
   return uniqueArrayItems.size === array.length;
 };
 
-export const isUniqueArrayItemWithNewEntry = (array: string[], name: string): boolean => isUniqueArrayItem([...array, name]);
+export const isUniqueArrayItemWithNewEntry = (array: string[], entry: string, excludeEntry?: string | false | undefined): boolean => {
+  let arrayNames = [...array, entry];
+
+  if (excludeEntry) arrayNames = arrayNames.filter((v) => v !== excludeEntry);
+
+  return isUniqueArrayItem(arrayNames);
+};
