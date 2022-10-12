8ee7a0a7cf50e3fdefba2ff5ad03ce4227df0897
badreleglil <badre.leglil@gmail.com>
2022-10-12 11:31:11 +0200

    Fix: get Node By name

--- src/utils/tree/getNodeByNameInCurrentTree/test/mock.ts
+++ src/utils/tree/getNodeByNameInCurrentTree/test/mock.ts
@@ -241,36 +241,48 @@ export const treeWithTreeMatchTree: Mock = {
   input: treeWithTree,
   output: {
     attributes: {
-      depth: 0,
-      isRoot: true,
-      label: "Nom de la machine",
-      type: "text",
-    },
-    children: [
-      {
+      depth: 2,
+      isDecision: true,
+      isLeaf: true,
+      label: "TreeCatalog",
+      tree: {
         attributes: {
-          depth: 1,
-          isLeaf: true,
-          label: "Type",
-          type: "select",
-          values: [
-            {
-              id: "0",
-              label: "Mini-pelle",
-              value: "minipelle",
-            },
-            {
-              id: "1",
-              label: "Nacelle",
-              value: "nacelle",
-            },
-          ],
+          depth: 0,
+          isRoot: true,
+          label: "Nom de la machine",
+          type: "text",
         },
-        children: [],
-        name: "type",
+        children: [
+          {
+            attributes: {
+              depth: 1,
+              isLeaf: true,
+              label: "Type",
+              type: "select",
+              values: [
+                {
+                  id: "0",
+                  label: "Mini-pelle",
+                  value: "minipelle",
+                },
+                {
+                  id: "1",
+                  label: "Nacelle",
+                  value: "nacelle",
+                },
+              ],
+            },
+            children: [],
+            name: "type",
+          },
+        ],
+        name: "machineName",
       },
-    ],
-    name: "machineName",
+      treePath: "/treeCatalog",
+      type: "tree",
+    },
+    children: [],
+    name: "treeCatalog",
   },
   searchName: "treeCatalog",
 };
