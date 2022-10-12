dfcebe0953fbf5ffe24f6d3fc70a8a128ea00ac6
mickaelaustoni <mickael.austoni@gmail.com>
2022-10-12 20:54:56 +0200

    refactor : backend language detector to local

--- src/config/i18n.ts
+++ src/config/i18n.ts
@@ -1,10 +1,16 @@
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-import Backend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
+import buttonEn from "@/locales/en/button.json";
+import formEn from "@/locales/en/form.json";
+import modalEn from "@/locales/en/modal.json";
+import translationEn from "@/locales/en/translation.json";
+import buttonFr from "@/locales/fr/button.json";
+import formFr from "@/locales/fr/form.json";
+import modalFr from "@/locales/fr/modal.json";
+import translationFr from "@/locales/fr/translation.json";
 
 i18n
-  .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
@@ -17,6 +23,21 @@ i18n
     react: {
       useSuspense: false,
     },
-  });
+    resources: {
+      en: {
+        button: buttonEn,
+        form: formEn,
+        modal: modalEn,
+        translation: translationEn,
+      },
+      fr: {
+        button: buttonFr,
+        form: formFr,
+        modal: modalFr,
+        translation: translationFr,
+      },
+    },
+  })
+  .then();
 
 export default i18n;
