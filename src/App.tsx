dcbe12b61a6a3924445eaad62be7f9d1fce262ae
badreleglil <badre.leglil@gmail.com>
2022-10-24 08:53:54 +0200

    Remove Token en end point props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import Treege from "@/features/Treege";
 
-const App = () => (
-  <Treege
-    authToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiYWRyZSIsImV4cCI6MTY2NjM2NDIxN30.PNOxrkWyNXuQfgqFYdi-cQhriISrX-oPSJSHrYvLVpc"
-    endPoint="https://stg.api.tracktor.fr"
-  />
-);
+const App = () => <Treege />;
 
 export default App;
