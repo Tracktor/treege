57aafddff259cbc1c94953bc68e9160334a788e6
badreleglil <badre.leglil@gmail.com>
2022-11-04 13:02:59 +0100

    Remove token & endPoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 import Treege from "@/features/Treege";
 
-const App = () => (
-  <Treege
-    authToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJiYWRyZSIsImV4cCI6MTY2NzY0NTMwMX0.BIhPljvs7FtL6tbF2I6pnOhLe2sCddgpWkkaW2jLUVA"
-    endPoint="https://stg.api.tracktor.fr"
-  />
-);
+const App = () => <Treege />;
 
 export default App;
