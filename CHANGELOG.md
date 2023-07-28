84aac0effafb42cfd25754e8c8cf84c4a5332fab
mickaelaustoni <mickael.austoni@gmail.com>
2023-07-28 11:44:35 +0200

    build: improvement

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,5 @@
 # [Versions](https://github.com/Tracktor/treege/releases)
 
-## v0.10.3
-- **[build]** : fix type entry
+## v0.10.4
+- **[build]** : exclude unused `public` directory from build
+- **[build]** : exclude unused `App.tsx` from build
--- vite.config.ts
+++ vite.config.ts
@@ -4,44 +4,55 @@ import dts from "vite-plugin-dts";
 import { defineConfig } from "vitest/config";
 import { dependencies, name } from "./package.json";
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: resolve(__dirname, "src/main.ts"),
-      fileName: "[name]",
-      formats: ["umd", "es"],
-      name,
-    },
-    rollupOptions: {
-      external: [...Object.keys(dependencies)],
-      output: {
-        globals: {
-          "@codemirror/lang-json": "codemirrorLangJson",
-          "@tanstack/react-query": "reactQuery",
-          "@tracktor/design-system": "tracktorDesignSystem",
-          "@uiw/codemirror-theme-dracula": "codemirrorThemeDracula",
-          "@uiw/react-codemirror": "reactCodemirror",
-          axios: "axios",
-          i18next: "i18next",
-          "i18next-browser-languagedetector": "i18nextBrowserLanguageDetector",
-          react: "React",
-          "react-d3-tree": "ReactD3Tree",
-          "react-dom": "ReactDOM",
-          "react-i18next": "reactI18next",
+// https://vitejs.dev/config/
+const config = ({ mode }) =>
+  defineConfig({
+    build: {
+      lib: {
+        entry: resolve(__dirname, "src/main.ts"),
+        fileName: "[name]",
+        formats: ["umd", "es"],
+        name,
+      },
+      rollupOptions: {
+        external: ["public/**/*", ...Object.keys(dependencies)],
+        output: {
+          globals: {
+            "@codemirror/lang-json": "codemirrorLangJson",
+            "@tanstack/react-query": "reactQuery",
+            "@tracktor/design-system": "tracktorDesignSystem",
+            "@uiw/codemirror-theme-dracula": "codemirrorThemeDracula",
+            "@uiw/react-codemirror": "reactCodemirror",
+            axios: "axios",
+            i18next: "i18next",
+            "i18next-browser-languagedetector": "i18nextBrowserLanguageDetector",
+            react: "React",
+            "react-d3-tree": "ReactD3Tree",
+            "react-dom": "ReactDOM",
+            "react-i18next": "reactI18next",
+          },
         },
       },
     },
-  },
-  plugins: [dts({ insertTypesEntry: true }), react()],
-  resolve: {
-    alias: [
-      { find: "@", replacement: resolve(__dirname, "src") },
-      { find: "~", replacement: resolve(__dirname) },
+    plugins: [
+      dts({
+        exclude: "src/App.tsx",
+        insertTypesEntry: true,
+      }),
+      react(),
     ],
-  },
-  test: {
-    environment: "jsdom",
-    globals: true,
-    setupFiles: "src/config/test.config.ts",
-  },
-});
+    publicDir: mode === "production" ? false : undefined,
+    resolve: {
+      alias: [
+        { find: "@", replacement: resolve(__dirname, "src") },
+        { find: "~", replacement: resolve(__dirname) },
+      ],
+    },
+    test: {
+      environment: "jsdom",
+      globals: true,
+      setupFiles: "src/config/test.config.ts",
+    },
+  });
+
+export default config;
