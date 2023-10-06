b466bb3641dd5eff0aea3795e8125a2c59cfc641
badreleglil <badre.leglil@gmail.com>
2023-10-06 14:17:49 +0200

    feat(tag):
    useCallback

--- src/features/Treege/components/Forms/FormTreeCardMutation/useFormTreeCardMutation.ts
+++ src/features/Treege/components/Forms/FormTreeCardMutation/useFormTreeCardMutation.ts
@@ -1,6 +1,6 @@
 import type { SelectChangeEvent } from "@tracktor/design-system";
 import type { HierarchyPointNode } from "d3-hierarchy";
-import { ChangeEvent, FormEvent, MouseEvent, SyntheticEvent, useEffect, useMemo, useState } from "react";
+import { ChangeEvent, FormEvent, MouseEvent, SyntheticEvent, useCallback, useEffect, useMemo, useState } from "react";
 import { useTranslation } from "react-i18next";
 import fields from "@/constants/fields";
 import { appendTreeCard, replaceTreeCard } from "@/features/Treege/reducer/treeReducer";
@@ -44,7 +44,7 @@ const useFormTreeCardMutation = () => {
   const isRequiredDisabled = fields.some((field) => field.type === type && field?.isRequiredDisabled);
   const isRepeatableDisabled = fields.some((field) => field.type === type && field?.isRepeatableDisabled);
   const isLeaf = currentHierarchyPointNode?.data?.attributes?.isLeaf ?? true;
-  const getDisabledValueField = (index: number) => !isDecisionField && index > 0;
+  const getDisabledValueField = useCallback((index: number) => !isDecisionField && index > 0, [isDecisionField]);
 
   const { refetch: fetchWorkflow, isInitialLoading: isWorkflowLoading } = useWorkflowQuery(treeSelected, {
     enabled: false,
@@ -53,7 +53,7 @@ const useFormTreeCardMutation = () => {
     },
   });
 
-  const handlePresetValues = (event: ChangeEvent<HTMLInputElement>, predicate: "value" | "label" | "message") => {
+  const handlePresetValues = useCallback((event: ChangeEvent<HTMLInputElement>, predicate: "value" | "label" | "message") => {
     setValues((prevState) =>
       prevState.map((item) => {
         const { id } = item;
@@ -68,110 +68,128 @@ const useFormTreeCardMutation = () => {
         return { ...item };
       })
     );
-  };
+  }, []);
 
-  const handleChangeTag = (_: SyntheticEvent<Element, Event>, newValue: string | { inputValue: string; label: string } | null) => {
-    if (typeof newValue === "string") {
-      setTag(newValue);
-    } else if (newValue && newValue.inputValue) {
-      // Create a new value from the user input
-      setTag(newValue.inputValue);
-    }
-  };
+  const handleChangeTag = useCallback(
+    (_: SyntheticEvent<Element, Event>, newValue: string | { inputValue: string; label: string } | null) => {
+      if (typeof newValue === "string") {
+        setTag(newValue);
+      } else if (newValue && newValue.inputValue) {
+        // Create a new value from the user input
+        setTag(newValue.inputValue);
+      }
+    },
+    []
+  );
 
-  const handleChangeHiddenValue = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeHiddenValue = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setHiddenValue(event.target.value);
-  };
+  }, []);
 
-  const handleChangeLabel = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeLabel = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setLabel(event.target.value);
-  };
+  }, []);
 
-  const handleChangeOptionLabel = (event: ChangeEvent<HTMLInputElement>) => {
-    handlePresetValues(event, "label");
-  };
+  const handleChangeOptionLabel = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      handlePresetValues(event, "label");
+    },
+    [handlePresetValues]
+  );
 
-  const handleChangeOptionValue = (event: ChangeEvent<HTMLInputElement>) => {
-    handlePresetValues(event, "value");
-  };
+  const handleChangeOptionValue = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      handlePresetValues(event, "value");
+    },
+    [handlePresetValues]
+  );
 
-  const handleChangeOptionMessage = (event: ChangeEvent<HTMLInputElement>) => {
-    handlePresetValues(event, "message");
-  };
+  const handleChangeOptionMessage = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      handlePresetValues(event, "message");
+    },
+    [handlePresetValues]
+  );
 
-  const handleChangeStep = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeStep = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setStep(event.target.value);
-  };
+  }, []);
 
-  const handleChangeName = (event: ChangeEvent<HTMLInputElement>) => {
-    const { value } = event.target;
+  const handleChangeName = useCallback(
+    (event: ChangeEvent<HTMLInputElement>) => {
+      const { value } = event.target;
 
-    setName(event.target.value);
+      setName(event.target.value);
 
-    if (!tree || !value) {
-      setUniqueNameErrorMessage("");
-      return;
-    }
+      if (!tree || !value) {
+        setUniqueNameErrorMessage("");
+        return;
+      }
 
-    const excludeNameOnEditModal = isEditModal && currentHierarchyPointNode?.data.name;
-    const currentTreePath = treePath?.at(-1)?.path;
-    const currentTree = getTree(tree, currentTreePath);
-    const arrayNames = getTreeNames(currentTree);
-    const isUnique = isUniqueArrayItemWithNewEntry(arrayNames, value, excludeNameOnEditModal);
+      const excludeNameOnEditModal = isEditModal && currentHierarchyPointNode?.data.name;
+      const currentTreePath = treePath?.at(-1)?.path;
+      const currentTree = getTree(tree, currentTreePath);
+      const arrayNames = getTreeNames(currentTree);
+      const isUnique = isUniqueArrayItemWithNewEntry(arrayNames, value, excludeNameOnEditModal);
 
-    if (isUnique) {
-      setUniqueNameErrorMessage("");
-      return;
-    }
+      if (isUnique) {
+        setUniqueNameErrorMessage("");
+        return;
+      }
 
-    setUniqueNameErrorMessage(t("mustBeUnique", { ns: "form" }));
-  };
+      setUniqueNameErrorMessage(t("mustBeUnique", { ns: "form" }));
+    },
+    [currentHierarchyPointNode?.data.name, isEditModal, t, tree, treePath]
+  );
 
-  const handleChangeRequired = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeRequired = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setRequired(event.target.checked);
-  };
+  }, []);
 
-  const handleChangeIsDecisionField = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeIsDecisionField = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setIsDecision(event.target.checked);
-  };
+  }, []);
 
-  const handleChangeRepeatable = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeRepeatable = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setRepeatable(event.target.checked);
-  };
+  }, []);
 
-  const handleChangeType = (event: SelectChangeEvent<TreeNodeField["type"]>) => {
+  const handleChangeType = useCallback((event: SelectChangeEvent<TreeNodeField["type"]>) => {
     setType(event.target.value as TreeNodeField["type"]);
 
     setIsDecision(false);
     setRequired(false);
     setRepeatable(false);
-  };
+  }, []);
 
-  const handleChangeTreeSelect = (event: SelectChangeEvent) => {
+  const handleChangeTreeSelect = useCallback((event: SelectChangeEvent) => {
     setTreeSelected(event.target.value);
-  };
+  }, []);
 
-  const handleChangeMessage = (nameMessage: "on" | "off") => (event: ChangeEvent<HTMLInputElement>) => {
-    setMessages((currentState) => ({ ...currentState, [nameMessage]: event.target.value }));
-  };
+  const handleChangeMessage = useCallback(
+    (nameMessage: "on" | "off") => (event: ChangeEvent<HTMLInputElement>) => {
+      setMessages((currentState) => ({ ...currentState, [nameMessage]: event.target.value }));
+    },
+    []
+  );
 
-  const handleChangeHelperText = (event: ChangeEvent<HTMLInputElement>) => {
+  const handleChangeHelperText = useCallback((event: ChangeEvent<HTMLInputElement>) => {
     setHelperText(event.target.value);
-  };
+  }, []);
 
-  const handleAddValue = () => {
+  const handleAddValue = useCallback(() => {
     setValues((prevState) => {
       const lastId = Number(prevState[prevState.length - 1].id);
       const nextId = String(lastId + 1);
       return [...prevState, { ...defaultValues[0], id: nextId }];
     });
-  };
+  }, [defaultValues]);
 
-  const handleDeleteValue = (e: MouseEvent<HTMLButtonElement>) => {
+  const handleDeleteValue = useCallback((e: MouseEvent<HTMLButtonElement>) => {
     setValues((prevState) => prevState.filter(({ id }) => e.currentTarget.value !== id));
-  };
+  }, []);
 
-  const getNestedChildren = (hierarchyPointNode: null | HierarchyPointNode<TreeNode>, index: number) => {
+  const getNestedChildren = useCallback((hierarchyPointNode: null | HierarchyPointNode<TreeNode>, index: number) => {
     const nestedChildren = hierarchyPointNode?.children?.[index]?.data?.children;
 
     if (!nestedChildren) {
@@ -183,97 +201,133 @@ const useFormTreeCardMutation = () => {
       children,
       name: childrenName,
     }));
-  };
+  }, []);
 
-  const getChildren = (depth: number) => {
-    if (!isDecision) {
-      return [];
-    }
+  const getChildren = useCallback(
+    (depth: number) => {
+      if (!isDecision) {
+        return [];
+      }
 
-    return values
-      ?.filter((_, index) => !getDisabledValueField(index)) // filter disabled value
-      ?.map(({ message, value, label: optionLabel }, index) => {
-        const nextName = `${name}:${value}`;
-        const children = getNestedChildren(currentHierarchyPointNode, index);
+      return values
+        ?.filter((_, index) => !getDisabledValueField(index)) // filter disabled value
+        ?.map(({ message, value, label: optionLabel }, index) => {
+          const nextName = `${name}:${value}`;
+          const children = getNestedChildren(currentHierarchyPointNode, index);
 
-        return {
-          attributes: {
-            depth: depth + 1,
-            label: optionLabel,
-            value,
-            ...(message && { message }),
-            ...(children.length === 0 && { isLeaf: true }),
-          },
-          children,
-          name: nextName,
-        };
-      });
-  };
+          return {
+            attributes: {
+              depth: depth + 1,
+              label: optionLabel,
+              value,
+              ...(message && { message }),
+              ...(children.length === 0 && { isLeaf: true }),
+            },
+            children,
+            name: nextName,
+          };
+        });
+    },
+    [currentHierarchyPointNode, getDisabledValueField, getNestedChildren, isDecision, name, values]
+  );
 
-  const getTreeValuesWithoutEmptyMessage = (valuesData: TreeValues[]) =>
-    valuesData.map(({ message, ...rest }) => ({ ...rest, ...(message && { message }) }));
+  const getTreeValuesWithoutEmptyMessage = useCallback(
+    (valuesData: TreeValues[]) => valuesData.map(({ message, ...rest }) => ({ ...rest, ...(message && { message }) })),
+    []
+  );
 
-  const getWorkFlowReq = (isTreeSelected: boolean, isEdit: boolean, isOtherTree: boolean) => {
-    // make request if is Tree and not update or isOtherTree selected
-    if (isTreeSelected && (!isEdit || isOtherTree)) {
-      return fetchWorkflow();
-    }
+  const getWorkFlowReq = useCallback(
+    (isTreeSelected: boolean, isEdit: boolean, isOtherTree: boolean) => {
+      // make request if is Tree and not update or isOtherTree selected
+      if (isTreeSelected && (!isEdit || isOtherTree)) {
+        return fetchWorkflow();
+      }
 
-    // don't edit tree
-    if (isTreeSelected && isEdit && !isOtherTree) {
-      return { data: { workflow: currentHierarchyPointNode?.data.attributes?.tree }, isError: null };
-    }
+      // don't edit tree
+      if (isTreeSelected && isEdit && !isOtherTree) {
+        return { data: { workflow: currentHierarchyPointNode?.data.attributes?.tree }, isError: null };
+      }
 
-    return { data: null, isError: null };
-  };
+      return { data: null, isError: null };
+    },
+    [currentHierarchyPointNode?.data.attributes?.tree, fetchWorkflow]
+  );
 
-  const handleSubmit = async (e: FormEvent) => {
-    e.preventDefault();
+  const handleSubmit = useCallback(
+    async (e: FormEvent) => {
+      e.preventDefault();
 
-    const { on, off } = messages;
-    const currentName = currentHierarchyPointNode?.data?.name || "";
-    const currentDepth = currentHierarchyPointNode?.depth || 0;
-    const isEdit = modalOpen === "edit";
-    const depth = currentDepth + (isEdit || currentHierarchyPointNode === null ? 0 : 1);
-    const childOfChildren = getChildren(depth);
-    const currentPath = treePath?.at(-1)?.path;
-    const newPath = treePath.length ? `${currentPath}/${name}` : `/${name}`;
-    const isOtherTree = currentHierarchyPointNode?.data.attributes?.tree?.treeId !== treeSelected;
+      const { on, off } = messages;
+      const currentName = currentHierarchyPointNode?.data?.name || "";
+      const currentDepth = currentHierarchyPointNode?.depth || 0;
+      const isEdit = modalOpen === "edit";
+      const depth = currentDepth + (isEdit || currentHierarchyPointNode === null ? 0 : 1);
+      const childOfChildren = getChildren(depth);
+      const currentPath = treePath?.at(-1)?.path;
+      const newPath = treePath.length ? `${currentPath}/${name}` : `/${name}`;
+      const isOtherTree = currentHierarchyPointNode?.data.attributes?.tree?.treeId !== treeSelected;
 
-    const { data: workflow, isError } = await getWorkFlowReq(isTreeField, isEdit, isOtherTree);
+      const { data: workflow, isError } = await getWorkFlowReq(isTreeField, isEdit, isOtherTree);
 
-    if (isError) return;
+      if (isError) return;
 
-    const children = {
-      attributes: {
-        depth,
-        label,
-        type,
-        ...(helperText && { helperText }),
-        ...((off || on) && {
-          messages: { ...(off && { off }), ...(on && { on }) },
-        }),
-        ...(isTreeField && { tree: { ...workflow?.workflow, treeId: treeSelected } as TreeNode, treePath: newPath }),
-        ...(isDecision && { isDecision }),
-        ...(isDecisionField && !isDecision && { values: getTreeValuesWithoutEmptyMessage(values) }),
-        ...(required && { required }),
-        ...(step && { step }),
-        ...(repeatable && { repeatable }),
-        ...(isHiddenField && { hiddenValue }),
-        ...(tag && { tag }),
-      },
-      children: childOfChildren,
+      const children = {
+        attributes: {
+          depth,
+          label,
+          type,
+          ...(helperText && { helperText }),
+          ...((off || on) && {
+            messages: { ...(off && { off }), ...(on && { on }) },
+          }),
+          ...(isTreeField && { tree: { ...workflow?.workflow, treeId: treeSelected } as TreeNode, treePath: newPath }),
+          ...(isDecision && { isDecision }),
+          ...(isDecisionField && !isDecision && { values: getTreeValuesWithoutEmptyMessage(values) }),
+          ...(required && { required }),
+          ...(step && { step }),
+          ...(repeatable && { repeatable }),
+          ...(isHiddenField && { hiddenValue }),
+          ...(tag && { tag }),
+        },
+        children: childOfChildren,
+        name,
+      };
+
+      if (isEdit) {
+        dispatchTree(replaceTreeCard(currentPath || "", currentName, children));
+      } else {
+        dispatchTree(appendTreeCard(currentPath || null, currentName, children));
+      }
+
+      setModalOpen(null);
+    },
+    [
+      currentHierarchyPointNode,
+      dispatchTree,
+      getChildren,
+      getTreeValuesWithoutEmptyMessage,
+      getWorkFlowReq,
+      helperText,
+      hiddenValue,
+      isDecision,
+      isDecisionField,
+      isHiddenField,
+      isTreeField,
+      label,
+      messages,
+      modalOpen,
       name,
-    };
-
-    if (isEdit) {
-      dispatchTree(replaceTreeCard(currentPath || "", currentName, children));
-    } else {
-      dispatchTree(appendTreeCard(currentPath || null, currentName, children));
-    }
-
-    setModalOpen(null);
-  };
+      repeatable,
+      required,
+      setModalOpen,
+      step,
+      tag,
+      treePath,
+      treeSelected,
+      type,
+      values,
+    ]
+  );
 
   // Set initial form data edit modal
   useEffect(() => {
