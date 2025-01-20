1bc34158c10f73e3784ae6e8f18633cb7292c1c2
Mibiki <108873902+MickaelAustoni@users.noreply.github.com>
2025-01-20 11:18:38 +0100

    feat: logo gradient neon

--- src/components/DataDisplay/Logo/Logo.tsx
+++ src/components/DataDisplay/Logo/Logo.tsx
@@ -1,5 +1,24 @@
+import { Box } from "@tracktor/design-system";
 import LogoSvg from "@/assets/img/treege-white.svg";
 
-const Logo = () => <img src={LogoSvg} alt="Treege" height={30} width="auto" />;
+const Logo = () => (
+  <Box
+    sx={{
+      position: "relative",
+    }}
+  >
+    <Box
+      sx={{
+        background: ({ palette }) => `radial-gradient(circle, ${palette.primary.main}80 0%, ${palette.primary.main}00 70%)`,
+        filter: "blur(12px)",
+        height: 30,
+        position: "absolute",
+        transform: "scale(1.2)",
+        width: 30,
+      }}
+    />
+    <img src={LogoSvg} alt="Treege" height={30} width="auto" />
+  </Box>
+);
 
 export default Logo;
