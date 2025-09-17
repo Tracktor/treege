36cdf0bfdde9b7679a689eef528dafcf464d0d7c
kevin <kevin.graff1@gmail.com>
2025-09-17 08:50:37 +0200

    feat: enhance TextNode component with improved layout and child node handling

--- src/features/Treege/TreegeFlow/Nodes/Text.tsx
+++ src/features/Treege/TreegeFlow/Nodes/Text.tsx
@@ -1,31 +1,23 @@
 import AddIcon from "@mui/icons-material/Add";
-import { Card, CardContent, Chip, Box, Typography, IconButton } from "@tracktor/design-system";
+import { Card, CardContent, Chip, Box, Typography, IconButton, Stack } from "@tracktor/design-system";
 import { Position, Handle, useNodeConnections, type NodeProps, type Node } from "@xyflow/react";
 import { memo } from "react";
 import colors from "@/constants/colors";
 import { CustomNodeData } from "@/features/Treege/TreegeFlow/Nodes/nodeTypes";
 
 const TextNode = ({ id, data }: NodeProps<Node<CustomNodeData>>) => {
+  const parentConnections = useNodeConnections({ handleType: "target" });
   const { name, order, onAddNode } = data;
 
-  const parentConnections = useNodeConnections({ handleType: "target" });
-  const childConnections = useNodeConnections({ handleType: "source" });
-
-  const isLastNode = childConnections.length === 0;
-
   const handleAddChild = () => {
-    onAddNode?.(id); // hook will auto pick child if needed
-  };
-
-  const handleInsertChild = () => {
-    onAddNode?.(id); // hook auto picks the correct child
+    onAddNode?.(id);
   };
 
   return (
     <Box component="div">
       {parentConnections.length !== 0 && <Handle type="target" position={Position.Top} />}
 
-      <Card sx={{ background: colors.background, borderRadius: "1rem" }}>
+      <Card sx={{ background: colors.background, borderColor: colors.primary, borderRadius: "1rem" }}>
         <CardContent
           sx={{
             display: "flex",
@@ -35,25 +27,17 @@ const TextNode = ({ id, data }: NodeProps<Node<CustomNodeData>>) => {
             width: 200,
           }}
         >
-          <Typography variant="h5">
-            {name} (#{order})
-          </Typography>
-          <Chip color="primary" size="small" label="text" />
-          <Box sx={{ display: "flex", gap: 1 }}>
-            {isLastNode && (
+          <Stack spacing={2} alignItems="flex-end">
+            <Typography variant="h5">
+              {name} (#{order})
+            </Typography>
+            <Chip color="info" size="small" label="text" />
+            <Box sx={{ display: "flex", gap: 1 }}>
               <IconButton size="small" color="primary" onClick={handleAddChild}>
                 <AddIcon />
-                add
               </IconButton>
-            )}
-
-            {!isLastNode && (
-              <IconButton size="small" color="secondary" onClick={handleInsertChild}>
-                <AddIcon />
-                insert
-              </IconButton>
-            )}
-          </Box>
+            </Box>
+          </Stack>
         </CardContent>
       </Card>
 
