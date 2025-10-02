3780b744a54215e85052020380d0b3c85d1ac431
mickaelaustoni <mickael.austoni@gmail.com>
2025-10-02 17:12:47 +0200

    feat: improvement

--- src/features/Treege/Panel/ActionsPanel.tsx
+++ src/features/Treege/Panel/ActionsPanel.tsx
@@ -2,19 +2,15 @@ import { Panel } from "@xyflow/react";
 import { ArrowRightFromLine, Download } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
-const ActionsPanel = () => {
-  console.log("a");
-
-  return (
-    <Panel position="top-right" className="flex gap-2">
-      <Button variant="outline" size="sm">
-        <Download /> Import
-      </Button>
-      <Button variant="outline" size="sm">
-        <ArrowRightFromLine /> Export
-      </Button>
-    </Panel>
-  );
-};
+const ActionsPanel = () => (
+  <Panel position="top-right" className="flex gap-2">
+    <Button variant="outline" size="sm">
+      <Download /> Import Json
+    </Button>
+    <Button variant="outline" size="sm">
+      <ArrowRightFromLine /> Export Json
+    </Button>
+  </Panel>
+);
 
 export default ActionsPanel;
