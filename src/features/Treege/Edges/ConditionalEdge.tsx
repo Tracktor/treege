ce409b5786961248ec23d2f02a7786b3c0705e45
mickaelaustoni <mickael.austoni@gmail.com>
2025-10-02 09:41:25 +0200

    feat: conditional edge (WIP)

--- src/features/Treege/Edges/ConditionalEdge.tsx
+++ src/features/Treege/Edges/ConditionalEdge.tsx
@@ -2,6 +2,9 @@ import { BaseEdge, EdgeLabelRenderer, type EdgeProps, getBezierPath } from "@xyf
 import { Waypoints } from "lucide-react";
 import { MouseEvent } from "react";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 
 const ConditionalEdge = ({ id, sourceX, sourceY, targetX, targetY, sourcePosition, targetPosition, markerEnd, style }: EdgeProps) => {
   const [edgePath, labelX, labelY] = getBezierPath({
@@ -32,9 +35,39 @@ const ConditionalEdge = ({ id, sourceX, sourceY, targetX, targetY, sourcePositio
             transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
           }}
         >
-          <Button variant="secondary" size="icon" className="size-8" onClick={onEdgeClick}>
-            <Waypoints />
-          </Button>
+          <Popover>
+            <PopoverTrigger asChild>
+              <Button variant="secondary" size="icon" className="size-8" onClick={onEdgeClick}>
+                <Waypoints />
+              </Button>
+            </PopoverTrigger>
+            <PopoverContent className="w-80">
+              <div className="grid gap-4">
+                <div className="space-y-2">
+                  <h4 className="leading-none font-medium">Title</h4>
+                  <p className="text-muted-foreground text-sm">Description.</p>
+                </div>
+                <div className="grid gap-2">
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="width">Width</Label>
+                    <Input id="width" defaultValue="100%" className="col-span-2 h-8" />
+                  </div>
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="maxWidth">Max. width</Label>
+                    <Input id="maxWidth" defaultValue="300px" className="col-span-2 h-8" />
+                  </div>
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="height">Height</Label>
+                    <Input id="height" defaultValue="25px" className="col-span-2 h-8" />
+                  </div>
+                  <div className="grid grid-cols-3 items-center gap-4">
+                    <Label htmlFor="maxHeight">Max. height</Label>
+                    <Input id="maxHeight" defaultValue="none" className="col-span-2 h-8" />
+                  </div>
+                </div>
+              </div>
+            </PopoverContent>
+          </Popover>
         </div>
       </EdgeLabelRenderer>
     </>
