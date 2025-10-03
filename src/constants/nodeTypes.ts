483f5c7b60cf8c877198b4183adef5f83cb7c055
mickaelaustoni <mickael.austoni@gmail.com>
2025-10-03 18:39:10 +0200

    feat: improvement

--- src/constants/nodeTypes.ts
+++ src/constants/nodeTypes.ts
@@ -1,5 +1,5 @@
 import FlowNode from "@/features/Treege/Nodes/FlowNode";
-import GroupNode from "@/features/Treege/Nodes/GroupeNode";
+import GroupNode from "@/features/Treege/Nodes/GroupNode";
 import InputNode from "@/features/Treege/Nodes/InputNode";
 import JsonNode from "@/features/Treege/Nodes/JsonNode";
 import UINode from "@/features/Treege/Nodes/UINode";
