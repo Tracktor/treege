efc39998a9d198b55a4812758bc589d44fab72a2
mickaelaustoni <mickael.austoni@gmail.com>
2025-10-07 15:16:49 +0200

    feat: improvement

--- src/features/Treege/Nodes/Layout/NodeWrapper.tsx
+++ src/features/Treege/Nodes/Layout/NodeWrapper.tsx
@@ -1,5 +1,11 @@
-import { ReactNode } from "react";
+import { PropsWithChildren } from "react";
+import { cn } from "@/lib/utils";
 
-const NodeWrapper = ({ children }: { children: ReactNode }) => <div className="react-flow__node__wrapper">{children}</div>;
+interface NodeWrapperProps extends PropsWithChildren {
+  inGroup: boolean;
+}
 
+const NodeWrapper = ({ children, inGroup }: NodeWrapperProps) => (
+  <div className={cn("react-flow__node__wrapper", inGroup ? "in-group" : "")}>{children}</div>
+);
 export default NodeWrapper;
