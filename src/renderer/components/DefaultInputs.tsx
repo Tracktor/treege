fc947d63da156bec5b81625ef2a8a71b8ca47f9f
mickaelaustoni <mickael.austoni@gmail.com>
2025-10-16 12:55:06 +0200

    feat: improvement

--- src/renderer/components/DefaultInputs.tsx
+++ src/renderer/components/DefaultInputs.tsx
@@ -75,7 +75,7 @@ export const DefaultSelectInput = ({ node }: InputRenderProps) => {
       </Label>
       <Select value={value || ""} onValueChange={(val) => setFieldValue(name, val)}>
         <SelectTrigger>
-          <SelectValue placeholder="Sélectionnez une option" />
+          <SelectValue placeholder="Select an option" />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
