a48993adf528c62d96d3bdcbf3b4f9fef278d46a
mickaelaustoni <mickael.austoni@gmail.com>
2025-10-16 23:55:50 +0200

    feat: improvement

--- src/renderer/components/web/DefaultGroup.tsx
+++ src/renderer/components/web/DefaultGroup.tsx
@@ -9,7 +9,7 @@ export const DefaultGroup = ({ node, children }: NodeRenderProps & { children: R
   return (
     <section className="mb-6 p-4 border rounded-lg ">
       {node.data.label && <h3 className="text-lg font-semibold mb-4">{getTranslatedLabel(node.data.label, language)}</h3>}
-      <div>{children}</div>
+      {children}
     </section>
   );
 };
