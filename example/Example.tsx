a042b49eb191239f35c876f23ab3a0ac95666b3a
mickaelaustoni <mickael.austoni@gmail.com>
2025-10-20 12:46:28 +0200

    chore: example

--- example/Example.tsx
+++ example/Example.tsx
@@ -1,6 +1,6 @@
 import { Edge, Node } from "@xyflow/react";
 import ExampleLayout from "~/example/ExampleLayout";
-import example from "~/example/json/example.json";
+import example from "~/example/json/treege.json";
 
 const Example = () => <ExampleLayout defaultFlow={example as { nodes: Node[]; edges: Edge[] }} />;
 
