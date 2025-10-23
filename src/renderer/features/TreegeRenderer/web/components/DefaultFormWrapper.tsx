9b471251b30beb6a73642b4ffa049de877a157e5
mickaelaustoni <mickael.austoni@gmail.com>
2025-10-23 17:27:07 +0200

    feat: improvement

--- src/renderer/features/TreegeRenderer/web/components/DefaultFormWrapper.tsx
+++ src/renderer/features/TreegeRenderer/web/components/DefaultFormWrapper.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, ReactNode } from "react";
 
 const DefaultFormWrapper = ({ children, onSubmit }: { children: ReactNode; onSubmit: (e: FormEvent) => void }) => (
-  <form onSubmit={onSubmit} className="max-w-2xl mx-auto p-6 gap-y-3">
+  <form onSubmit={onSubmit} className="max-w-2xl mx-auto gap-y-3">
     {children}
   </form>
 );
